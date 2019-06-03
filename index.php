3cac9fbdbcefc4ed343dd963d28466ec1fe932cc
nastya <asy.pankova@gmail.com>
2019-06-03 15:43:11 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+<head>
+
+    <title>Practice</title>
+</head>
+<body>
+<a href="pract.php">Вывод дерева</a>
+<br>
+<a href="pract1.php">Бинарный поиск</a>
+<br>
+<a href="oop.php">Builder</a>
+<br>
+<a href="oop1.php">FactoryMethod</a>
+<br>
+</body>
+</html>
+
+
+
